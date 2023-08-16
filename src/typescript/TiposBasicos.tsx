c8dc83048354090bebb697e67d44f81b739823fc
vhtoledo <querynetworking@gmail.com>
2023-08-16 12:42:58 -0300

    tipos basicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,8 +1,18 @@
 
 export const TiposBasicos = () => {
-  return (
-    <>
-      <h1>TiposBasicos</h1>  
-    </>
-  )
+
+    const nombre: string = 'Victor';
+    const edad: number = 35;
+    const estaActivo:boolean = false;
+
+    const poderes: (string|number)[] = []; // 'Velocidad','Volar','Respirar en el agua'
+    
+    return (
+        <>
+            <h3>Tipos básicos</h3>
+            { nombre }, { edad }, { (estaActivo) ? 'activo' : 'no activo' }
+            <br/>
+            { poderes.join(', ') }
+        </>
+    )
 }
