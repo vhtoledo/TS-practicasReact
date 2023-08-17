822677876356d416988dc6e71734bad6dca53a8f
vhtoledo <querynetworking@gmail.com>
2023-08-17 10:19:22 -0300

    objetos literales e interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { TiposBasicos } from "./typescript/TiposBasicos"
+import { ObjetosLiterales } from './typescript/ObjetosLiterales';
 
 const App = () => {
   return (
     <div className="mt-2">
       <h1>Introduccion a TS -React</h1>
-      <TiposBasicos/>
+      <hr/>
+      {/*<TiposBasicos/>*/}
+      <ObjetosLiterales/>
     </div>
   )
 }
---
+++ src/typescript/ObjetosLiterales.tsx
@@ -0,0 +1,35 @@
+
+interface Persona {
+    nombreCompleto: string;
+    edad: number;
+    direccion: Direccion
+}
+
+interface Direccion {
+    pais: string;
+    casaNo: number;
+}
+
+
+export const ObjetosLiterales = () => {
+
+    const persona: Persona = {
+        nombreCompleto: 'Victor',
+        edad: 35,
+        direccion: {
+            pais: 'Argentina',
+            casaNo: 615
+        }
+    }
+
+    return (
+        <>
+            <h3>Objetos Literales</h3>
+            <code>
+                <pre>
+                    { JSON.stringify( persona, null, 2 ) }
+                </pre>
+            </code>
+        </>
+    )
+}
